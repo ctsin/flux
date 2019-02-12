8efc036deea0b213f1014b69c491a49228502abc
邢贵志 <xinggz@zhongan.com.cn>
2019-02-12 16:38:03 +0800

    完成

--- src/index.js
+++ src/index.js
@@ -1,88 +1,62 @@
-const Dispatcher = () => {
-  const stores = [];
-  const register = store => {
-    if (!store || !store.update) {
-      throw new Error('要求 store 参数，且需配备 update 方法');
-    }
-
-    let consumers = [];
-
-    const change = () => {
-      consumers.forEach(consumer => {
-        consumer(store);
-      });
-    };
-
-    const subscribe = (consumer, noInit = false) => {
-      consumers = consumers.concat(consumer);
-      !noInit ? consumer.forEach(c => c(store)) : null;
-    };
-
-    stores.push({ store, change });
-
-    return subscribe;
-  };
-
-  const dispatch = action => {
-    (stores || []).forEach(({ store, change }) => {
-      store.update(action, change);
-    });
-  };
-
-  return { register, dispatch };
-};
-
-const dispatcher = Dispatcher();
-
-const createAction = type => {
-  if (!type) throw new Error(`请传入 action 的 type 参数`);
-
-  return payload => dispatcher.dispatch({ type, payload });
-};
-
-const createSubscriber = store => dispatcher.register(store);
-
-const View = (subscribeToStore, increase, decrease) => {
-  let value = null;
-  const el = document.querySelector('#counter');
-  const display = el.querySelector('#display');
-  const [increaseBtn, decreaseBtn] = Array.from(el.querySelectorAll('.button'));
-  const render = () => (display.innerHTML = value);
-  const updateState = store => (value = store.getValue());
-
-  subscribeToStore([updateState, render]);
-  increaseBtn.addEventListener('click', increase);
-  decreaseBtn.addEventListener('click', decrease);
-};
-
-const INCREASE = 'INCREASE';
-const DECREASE = 'DECREASE';
-
-const counterStore = () => ({
-  data: { value: 0 },
-  getValue() {
-    return this.data.value;
-  },
-  update(action, change) {
-    switch (action.type) {
-      case INCREASE:
-        this.data.value += 1;
-        break;
-      case DECREASE:
-        this.data.value -= 1;
-        break;
-      default:
-        void null;
-    }
-
-    change();
-  },
+const compose = (...fns) => args =>
+  fns.reduce((composed, fn) => fn(composed), args);
+const oneSecond = () => 1000;
+const getCurrentTime = () => new Date();
+const clear = () => console.clear();
+const log = message => console.log(message);
+const display = target => time => target(time);
+const HOURS = 'hours';
+const MINUTES = 'minutes';
+const SECONDS = 'seconds';
+const serializeClockTime = date => ({
+  [HOURS]: date.getHours(),
+  [MINUTES]: date.getMinutes(),
+  [SECONDS]: date.getSeconds(),
 });
+const civilianHours = clockTime => ({
+  ...clockTime,
+  [HOURS]: clockTime.hours > 12 ? clockTime.hours - 12 : clockTime.hours,
+});
+const appendAMPM = clockTime => ({
+  ...clockTime,
+  ampm: clockTime.hours >= 12 ? 'PM' : 'AM',
+});
+const prependZero = key => clockTime => ({
+  ...clockTime,
+  [key]: clockTime[key] < 10 ? `0${clockTime[key]}` : clockTime[key],
+});
+const formatClock = format => time =>
+  format
+    .replace('hh', time.hours)
+    .replace('mm', time.minutes)
+    .replace('ss', time.seconds)
+    .replace('tt', time.ampm);
 
-const counterStoreSubScriber = createSubscriber(counterStore());
-const actions = {
-  increase: createAction(INCREASE),
-  decrease: createAction(DECREASE),
-};
+const convertToCivilianTime = clockTime =>
+  compose(
+    appendAMPM,
+    civilianHours
+  )(clockTime);
 
-View(counterStoreSubScriber, actions.increase, actions.decrease);
+const doubleDigits = civilianTime =>
+  compose(
+    prependZero(HOURS),
+    prependZero(MINUTES),
+    prependZero(SECONDS)
+  )(civilianTime);
+
+const startTicking = () =>
+  setInterval(
+    compose(
+      clear,
+      getCurrentTime,
+      serializeClockTime,
+      convertToCivilianTime,
+      doubleDigits,
+      formatClock('hh:mm:ss tt'),
+      display(log)
+    ),
+    oneSecond()
+  );
+
+startTicking();
